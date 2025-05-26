791b37c79fad9287ef7d31a8df3f88ed73930591
Russ Ferriday <russ.ferriday@gmail.com>
2025-05-26 14:40:57 +0100

    fix: add index URL pattern to test configuration

--- django_deepface/tests/urls.py
+++ django_deepface/tests/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="django_deepface/index.html"), name="index"),
     path("", include("django_deepface.urls")),
 ]
 
