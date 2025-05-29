79c3dc73efaddd9673ba4fe29dce474b022217ba
Russ Ferriday <russ.ferriday@gmail.com>
2025-05-29 23:05:42 +0100

    Bump version to 0.0.3: Add signals support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="django-deepface",
-    version="0.1.0",
+    version="0.0.3",
     author="Your Name",
     author_email="your.email@example.com",
     description="Django app for face recognition authentication using DeepFace",
